6b6d68f1e75f9e9b0d7fdbc4dd1c84c6686ede46
TaeHee00 <kancth03@gmail.com>
2023-05-05 03:16:41 +0900

    Project File 생성

---
+++ src/main/java/com/DevHive/TukMeet/TukMeetApplication.java
@@ -0,0 +1,13 @@
+package com.DevHive.TukMeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TukMeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DevHive/TukMeet/TukMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevHive.TukMeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukMeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
